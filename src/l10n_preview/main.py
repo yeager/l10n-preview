7651a0d8be178b5ebda9b11251f1cb5c8c53a6f6
BOSSe <BOSSe@danielnylander.se>
2026-02-14 20:01:26 +0100

    chore: update Transifex org slug to danielnylander

--- src/l10n_preview/main.py
+++ src/l10n_preview/main.py
@@ -408,7 +408,7 @@ class L10nPreviewApp(Adw.Application):
             website="https://github.com/yeager/l10n-preview",
             issue_url="https://github.com/yeager/l10n-preview/issues",
             comments=_("A localization tool by Daniel Nylander"),
-            translator_credits=_("Translate this app: https://app.transifex.com/linguaedit/l10n-preview/"),
+            translator_credits=_("Translate this app: https://app.transifex.com/danielnylander/l10n-preview/"),
         )
         about.present()
 
