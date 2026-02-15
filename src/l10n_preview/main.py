05394ac132a9af4dbf91dda3de7bbf5684820e05
BOSSe <BOSSe@danielnylander.se>
2026-02-15 14:54:56 +0100

    Add theme toggle, keyboard shortcuts, status bar

--- src/l10n_preview/main.py
+++ src/l10n_preview/main.py
@@ -12,6 +12,7 @@ gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw, Gio, GLib, Pango, Gdk  # noqa: E402
 
 from .po_parser import parse_file, TranslationEntry, EntryState  # noqa: E402
+from datetime import datetime as _dt_now
 
 # i18n setup
 APP_ID = "se.danielnylander.l10n-preview"
@@ -198,6 +199,12 @@ class L10nPreviewWindow(Adw.ApplicationWindow):
         menu_btn = Gtk.MenuButton(icon_name="open-menu-symbolic", menu_model=menu)
         header.pack_end(menu_btn)
 
+        # Theme toggle
+        self._theme_btn = Gtk.Button(icon_name="weather-clear-night-symbolic",
+                                     tooltip_text="Toggle dark/light theme")
+        self._theme_btn.connect("clicked", self._on_theme_toggle)
+        header.pack_end(self._theme_btn)
+
         # Search toggle
         self.search_btn = Gtk.ToggleButton(icon_name="system-search-symbolic")
         self.search_btn.set_tooltip_text(_("Search"))
@@ -266,6 +273,13 @@ class L10nPreviewWindow(Adw.ApplicationWindow):
         drop_target = Gtk.DropTarget.new(Gio.File, Gdk.DragAction.COPY)
         drop_target.connect("drop", self._on_drop)
         self.add_controller(drop_target)
+
+        # Status bar
+        self._status_bar = Gtk.Label(label="", halign=Gtk.Align.START,
+                                     margin_start=12, margin_end=12, margin_bottom=4)
+        self._status_bar.add_css_class("dim-label")
+        self._status_bar.add_css_class("caption")
+        main_box.append(self._status_bar)
 
         self.set_content(main_box)
 
@@ -382,6 +396,20 @@ class L10nPreviewWindow(Adw.ApplicationWindow):
             self.list_box.append(placeholder)
 
 
+
+    def _on_theme_toggle(self, _btn):
+        sm = Adw.StyleManager.get_default()
+        if sm.get_color_scheme() == Adw.ColorScheme.FORCE_DARK:
+            sm.set_color_scheme(Adw.ColorScheme.FORCE_LIGHT)
+            self._theme_btn.set_icon_name("weather-clear-night-symbolic")
+        else:
+            sm.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+            self._theme_btn.set_icon_name("weather-clear-symbolic")
+
+    def _update_status_bar(self):
+        self._status_bar.set_text("Last updated: " + _dt_now.now().strftime("%Y-%m-%d %H:%M"))
+
+
 class L10nPreviewApp(Adw.Application):
     """Main application class."""
 
@@ -393,6 +421,31 @@ class L10nPreviewApp(Adw.Application):
         about_action = Gio.SimpleAction.new("about", None)
         about_action.connect("activate", self._on_about)
         self.add_action(about_action)
+
+    def do_startup(self):
+        Adw.Application.do_startup(self)
+        self.set_accels_for_action("app.quit", ["<Control>q"])
+        self.set_accels_for_action("app.refresh", ["F5"])
+        self.set_accels_for_action("app.shortcuts", ["<Control>slash"])
+        for n, cb in [("quit", lambda *_: self.quit()),
+                      ("refresh", lambda *_: self._do_refresh()),
+                      ("shortcuts", self._show_shortcuts_window)]:
+            a = Gio.SimpleAction.new(n, None); a.connect("activate", cb); self.add_action(a)
+
+    def _do_refresh(self):
+        w = self.get_active_window()
+        if w and hasattr(w, '_load_file'): w._load_file(None)
+
+    def _show_shortcuts_window(self, *_args):
+        win = Gtk.ShortcutsWindow(transient_for=self.get_active_window(), modal=True)
+        section = Gtk.ShortcutsSection(visible=True, max_height=10)
+        group = Gtk.ShortcutsGroup(visible=True, title="General")
+        for accel, title in [("<Control>q", "Quit"), ("F5", "Refresh"), ("<Control>slash", "Keyboard shortcuts")]:
+            s = Gtk.ShortcutsShortcut(visible=True, accelerator=accel, title=title)
+            group.append(s)
+        section.append(group)
+        win.add_child(section)
+        win.present()
 
     def do_activate(self):
         win = L10nPreviewWindow(application=self)
